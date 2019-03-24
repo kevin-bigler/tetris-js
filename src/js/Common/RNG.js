9133a73b7f646d57440551b7c3118a60789153a7
Kevin Bigler <kevinbigler@gmail.com>
2019-03-24 22:48:06 +0100

    Update comments in RNG class to be jsdocs

--- src/js/Common/RNG.js
+++ src/js/Common/RNG.js
@@ -1,13 +1,24 @@
 export default class RNG {
-
-	// min inclusive, max exclusive
+	/**
+	 * Get random integer value from a given range
+	 *
+	 * @param {number} min Inclusive
+	 * @param {number} max Exclusive
+	 * @returns {number} Integer value
+	 */
 	static getRandomInt(min, max) {
 	  min = Math.ceil(min);
 	  max = Math.floor(max);
 	  return Math.floor(Math.random() * (max - min)) + min;
 	}
 
-	// min & max inclusive
+	/**
+	 * min & max inclusive
+	 *
+	 * @param {number} min inclusive
+	 * @param {number} max inclusive
+	 * @returns {number}
+	 */
 	static getRandomIntInclusive(min, max) {
 	  min = Math.ceil(min);
 	  max = Math.floor(max);
